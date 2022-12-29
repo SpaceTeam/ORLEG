ddc1ad1cb7f3f081aa7cf0f4efe6495f6b87f4d5
Lukas10999 <38700149+Lukas10999@users.noreply.github.com>
2022-12-29 14:23:19 +0000

    Fixed install

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ sudo apt-get install gfortran -y
 sudo apt-get install python3-pip -y
 
 pip install pip-tools
-pip install numpy
 
 pip-compile requirements.in && pip-compile requirements-dev.in
 pip-sync requirements.txt && pip-sync requirements-dev.txt
